3b5a708cd0627064a1b2104d3abe0553e9aec035
Alexander Kiseliev <ack@CaH4aC>
2016-10-03 16:40:45 +0300

    renaming ui to themng in rails_admin sass generators upd; config upd for cancancan

--- app/models/hancock/embedded_element.rb
+++ app/models/hancock/embedded_element.rb
@@ -5,10 +5,10 @@ module Hancock
 
       include Hancock::Decorators::EmbeddedElement
 
-
-      # use it in inherited model
-      # rails_admin &Hancock::Admin::EmbeddedElement.config
-
+      rails_admin(&Hancock::Admin::EmbeddedElement.config.config(rails_admin_add_fields) { |config|
+        rails_admin_add_config(config)
+      })
+      
       # use it in rails_admin in parent model for sort
       # sort_embedded({fields: [:embedded_field_1, :embedded_field_2...]})
       # or u need to override rails_admin in inherited model to add sort field
