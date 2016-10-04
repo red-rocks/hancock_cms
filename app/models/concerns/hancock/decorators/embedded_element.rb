343905a02c800b09be3f1a3d3b61166c23efa980
Alexander Kiseliev <ack@CaH4aC>
2016-10-04 09:32:07 +0300

    typos

--- app/models/concerns/hancock/decorators/embedded_element.rb
+++ app/models/concerns/hancock/decorators/embedded_element.rb
@@ -7,22 +7,22 @@ module Hancock::Decorators
       # def image_styles
       #   super
       # end
-
+      #
       # def image_jcrop_options
       #   super
       # end
-
-      ############# rails_admin ##############
-      def self.rails_admin_add_fields
-        [] #super
-      end
-
-      def self.rails_admin_add_config(config)
-        #super(config)
-      end
+      #
+      # ############# rails_admin ##############
+      # def self.rails_admin_add_fields
+      #   [] #super
+      # end
+      #
+      # def self.rails_admin_add_config(config)
+      #   #super(config)
+      # end
 
     end
 
-    
+
   end
 end
--- lib/hancock/models/mongoid/embedded_element.rb
+++ lib/hancock/models/mongoid/embedded_element.rb
@@ -7,6 +7,7 @@ module Hancock
         included do
           field :name, type: String, localize: Hancock.configuration.localize, default: ""
         end
+        
       end
     end
   end
