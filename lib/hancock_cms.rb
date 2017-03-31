7eb36b57c04f8d16f770f1abcb00e577355532cd
Alexander Kiseliev <i43ack@gmail.com>
2017-03-31 13:56:32 +0300

    slim rails; hancock_array proto

--- lib/hancock_cms.rb
+++ lib/hancock_cms.rb
@@ -19,13 +19,13 @@ require_relative 'filename_to_slug'
 
 require 'codemirror-rails'
 
-require 'sprockets'
 
 require 'rails_admin'
 require 'hancock/rails_admin_ext/config'
 
 require 'hancock/rails_admin_ext/hancock_enum'
 require 'hancock/rails_admin_ext/hancock_enum_with_custom'
+require 'hancock/rails_admin_ext/hancock_array'
 require 'hancock/rails_admin_ext/hancock_hash'
 require 'hancock/rails_admin_ext/hancock_html'
 require 'hancock/rails_admin_ext/hancock_slugs'
