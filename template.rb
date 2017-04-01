5e97f1c4be25ebacfd6e468e68008c5031263a95
Alexander Kiseliev <i43ack@gmail.com>
2017-04-01 10:38:48 +0300

    rails versions upd

--- template.rb
+++ template.rb
@@ -2,8 +2,8 @@ rails_spec = (Gem.loaded_specs["railties"] || Gem.loaded_specs["rails"])
 version = rails_spec.version.to_s
 
 mongoid = options[:skip_active_record]
-actual_rails_version = "4.2.7.1"
-notsupported_rails_version = "5.0.0"
+actual_rails_version = "4.2.8"
+notsupported_rails_version = "5.x"
 
 if Gem::Version.new(version) < Gem::Version.new(actual_rails_version) or Gem::Version.new(version) >= Gem::Version.new(notsupported_rails_version)
   puts "You are using an incorrect version of Rails (#{version})"
