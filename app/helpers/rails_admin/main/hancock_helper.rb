277027daed34dfb388d027364cc0d67e1975bd01
Alexander Kiseliev <i43ack@gmail.com>
2017-08-02 13:14:25 +0300

    hancock_menu_for (rails_admin menu_for with titles)

--- app/helpers/rails_admin/main/hancock_helper.rb
+++ app/helpers/rails_admin/main/hancock_helper.rb
@@ -47,5 +47,21 @@ module RailsAdmin::Main
       end.join.html_safe
     end
 
+
+    def hancock_menu_for(parent, abstract_model = nil, object = nil, only_icon = false) # perf matters here (no action view trickery)
+      actions = actions(parent, abstract_model, object).select { |a| a.http_methods.include?(:get) }
+      actions.collect do |action|
+        wording = wording_for(:menu, action)
+        %(
+          <li title="#{wording}" rel="#{'tooltip' if only_icon}" class="icon #{action.key}_#{parent}_link #{'active' if current_action?(action)}">
+            <a class="#{action.pjax? ? 'pjax' : ''}" href="#{rails_admin.url_for(action: action.action_name, controller: 'rails_admin/main', model_name: abstract_model.try(:to_param), id: (object.try(:persisted?) && object.try(:id) || nil))}">
+              <i class="#{action.link_icon}"></i>
+              <span#{only_icon ? " style='display:none'" : ''}>#{wording}</span>
+            </a>
+          </li>
+        )
+      end.join.html_safe
+    end
+
   end
 end
