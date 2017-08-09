b74ba7d7903fad62e7eeb9514625c5291866845b
olegfineev <fineevoleg@gmail.com>
2017-08-09 16:55:37 +0300

    sidebar recolor | sidebar icons default | tables width | tables images

--- app/helpers/rails_admin/application/hancock_helper.rb
+++ app/helpers/rails_admin/application/hancock_helper.rb
@@ -7,7 +7,8 @@ module RailsAdmin::Application
         model_param = node.abstract_model.to_param
         url         = rails_admin.url_for(action: :index, controller: 'rails_admin/main', model_name: model_param)
         level_class = " nav-level-#{level}" if level > 0
-        nav_icon = node.navigation_icon ? %(<i class="#{node.navigation_icon}"></i>).html_safe : ''
+        # nav_icon = node.navigation_icon ? %(<i class="#{node.navigation_icon}"></i>).html_safe : ''
+        nav_icon = %(<i class="#{node.navigation_icon}"></i>).html_safe
         li = content_tag :li, data: {model: model_param, "name-synonyms": node.name_synonyms} do
           link_to nav_icon + capitalize_first_letter(node.label_plural), url, class: "pjax#{level_class}", title: capitalize_first_letter(node.label_plural)
         end
