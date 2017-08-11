b7bd781a9f489428fddbc67f86927a1204257229
Alexander Kiseliev <i43ack@gmail.com>
2017-08-11 10:12:52 +0300

    navigation fixes; need tests

--- app/models/hancock/embedded_element.rb
+++ app/models/hancock/embedded_element.rb
@@ -15,3 +15,21 @@ module Hancock
     end
   end
 end
+# 
+# counter = 0
+# 1_000_000.times do |i|
+#   i = i.to_s
+#   next if i.length < 6
+#   i = i.split("").uniq
+#   next if i.length < 6
+#   next if i.include?("0")
+#   next if i.include?("7")
+#   next if i.include?("8")
+#   next if i.include?("9")
+#   next if i[0] != '1'
+#   next if i[1] != '2'
+#   next if i[2] != '3'
+#   puts i.join
+#   counter += 1
+# end
+# puts counter
