e95f753c1489cd18e3f37768cc0cc8ba243593f8
Alexander Kiseliev <i43ack@gmail.com>
2017-08-21 15:02:19 +0300

    insertion REGEXP proto

--- lib/hancock/rails_admin_settings_patch.rb
+++ lib/hancock/rails_admin_settings_patch.rb
@@ -100,33 +100,49 @@ module Hancock
             searchable true
             weight 7
           end
-          field :raw do
+          field :raw_data do
             weight 8
-            searchable true
             pretty_value do
               if bindings[:object].file_kind?
                 "<a href='#{CGI::escapeHTML(bindings[:object].file.url)}'>#{CGI::escapeHTML(bindings[:object].to_path)}</a>".html_safe.freeze
               elsif bindings[:object].image_kind?
                 "<a href='#{CGI::escapeHTML(bindings[:object].file.url)}'><img src='#{CGI::escapeHTML(bindings[:object].file.url)}' /></a>".html_safe.freeze
+              elsif bindings[:object].array_kind?
+                (bindings[:object].raw_array || []).join("<br>").html_safe
+              elsif bindings[:object].hash_kind?
+                "<pre>#{JSON.pretty_generate(bindings[:object].raw_hash || {})}</pre>".html_safe
               else
                 value
               end
             end
           end
-          field :raw_array do
-            weight 9
-            searchable true
-            pretty_value do
-              (bindings[:object].raw_array || []).join("<br>").html_safe
-            end
-          end
-          field :raw_hash do
-            weight 10
-            searchable true
-            pretty_value do
-              "<pre>#{JSON.pretty_generate(bindings[:object].raw_hash || {})}</pre>".html_safe
-            end
-          end
+          # field :raw do
+          #   weight 8
+          #   searchable true
+          #   pretty_value do
+          #     if bindings[:object].file_kind?
+          #       "<a href='#{CGI::escapeHTML(bindings[:object].file.url)}'>#{CGI::escapeHTML(bindings[:object].to_path)}</a>".html_safe.freeze
+          #     elsif bindings[:object].image_kind?
+          #       "<a href='#{CGI::escapeHTML(bindings[:object].file.url)}'><img src='#{CGI::escapeHTML(bindings[:object].file.url)}' /></a>".html_safe.freeze
+          #     else
+          #       value
+          #     end
+          #   end
+          # end
+          # field :raw_array do
+          #   weight 9
+          #   searchable true
+          #   pretty_value do
+          #     (bindings[:object].raw_array || []).join("<br>").html_safe
+          #   end
+          # end
+          # field :raw_hash do
+          #   weight 10
+          #   searchable true
+          #   pretty_value do
+          #     "<pre>#{JSON.pretty_generate(bindings[:object].raw_hash || {})}</pre>".html_safe
+          #   end
+          # end
           field :cache_keys_str, :text do
             weight 11
             searchable true
